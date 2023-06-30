cec3018a5d6bce61479eb0efe5c04be355d28522
LettyMoila <moilankibelo@gmail.com>
2023-06-30 04:53:56 +0200

    started with home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,15 @@
 const Home = () =>{
     return(
-        <></>
+        <div className="h-screen w-screen bg-emerald-200">
+            <section className="h-24 w-full bg-emerald-500">
+                
+            </section>
+            <section className="flex gap-6 p-10 justify-center">
+            <div className="h-40 w-40 bg-emerald1500 rounded-md"></div>
+                <div className="h-40 w-40 bg-emerald-100 rounded-md"></div>
+                <div className="h-40 w-40 bg-emerald-100 rounded-md"></div>
+            </section>
+        </div>
     )
 }
 
