fb99503b65536f86fd77d1b421603ff2248c1b3f
LettyMoila <moilankibelo@gmail.com>
2023-07-11 15:12:58 +0200

    tackled landing and user registration page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,33 +2,54 @@ import React from 'react';
 import { Link } from 'react-router-dom'
 import logo from '../assets/Expense-logo.png'
 
-const Landing = () =>{
+const Landing = () => {
 
     function handleClick(){
         window.location.href = '/Register';
     }
     return(
         <div className="flex h-screen w-screen">
-            <div className="w-2/5 h-full bg-emerald-500">
+            <div className="w-2/5 h-full bg-emerald-500 grid gap-4">
                 <section className="flex w-full h-24 p-1.5">
                     <img src={logo} className="w-10 h-10"/>
                     <h1 className="text-left pl-3 text-3xl">Xpense</h1>
                 </section>
-                <section className="my-36 w-3/4">
-                    <p className="text-left pl-1.5">Get a clear full view of your spendings and know where to save. Xpense
+                <section className="my-36 w-4/5 p-8 text-left">
+                    <h1 className='text-5xl'>HAVE MORE MONEY! WHEN YOU BUDGET SMART</h1>
+                    <p className="text-xl pt-4">Get a clear full view of your spendings and know where to save. Xpense
                         is the new "at the tip of your finger" financial planner.
                     </p>
                 </section>
+                <section>
                 <button className="my-3 rounded-2xl w-32 hover:ring-blue-500">Get started</button>
-            </div>
-            <div className="w-3/5 h-full bg-emerald-200">
-                <section className="flex gap-4 align-end">
-                    <button><span>&#128075</span>Hi!</button>
-                    <Link to='/Login'>Login</Link>
-                    <button onClick={handleClick}>Sign up</button>
                 </section>
-                <section className="my-28 w-2/3 h-80 bg-white ml-14 rounded-lg rotate-12">
-                    healthy spending
+            </div>
+
+            <div className="w-3/5 h-full bg-emerald-200 ">
+                <section className="flex justify-between p-6">
+                    <section>
+                        <button className='rounded-full w-36 bg-emerald-200 border-2 border-emerald-500'> Hi there!</button>
+                    </section>
+                    <section className='flex gap-3'>
+                        <Link to='/Login'>Login</Link>
+                        <button className='rounded-full bg-emerald-500 w-24' onClick={handleClick} >Sign up</button>
+                    </section>  
+                </section>
+                <section className='flex items-center justify-center h-5/6'>
+                    <div className=" w-2/3 h-5/6 bg-white rounded-2xl rotate-12 p-4 grid gap-4">
+                        <div className='flex justify-between'>
+                            <h1 className='text-gray-300 text-lg'>your conversion</h1>
+                            <h1 className='text-red-500 text-lg'>Xpense</h1>
+                        </div>
+                        <div className='text-4xl'>
+                            <p>We do not promise this figure,
+                                but we promise you 45%</p>
+                        </div>
+                        <div className='h-80 bg-[url("/src/assets/lineGraph.svg")] bg-cover bg-no-repeat'></div>
+                        <div className=''>
+                            <h1 className='text-4xl text-left pl-6'>+99,9%</h1>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,28 @@
 const Register = () =>{
     return(
-        <></>
+        <div className="bg-emerald-200 h-screen w-screen">
+            <section>
+                <h1 className="text-4xl">signup here</h1>
+                <div className="grid grid-cols-4">
+                    <section>
+                        <label>Fullname</label><br/>
+                        <input type="text"/>
+                    </section>
+                    <section>
+                        <label>Email</label><br/>
+                        <input type="email"/>
+                    </section>
+                    <section>
+                        <label>Password</label><br/>
+                        <input type="password"/>
+                    </section>
+                    <section>
+                        <label>Confirm password</label><br/>
+                        <input type="password"/>
+                    </section>
+                </div>
+            </section>
+        </div>
     )
 }
 
