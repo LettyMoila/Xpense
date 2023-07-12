fca3a3bf6b8a2c45117a266222337716310c4139
LettyMoila <moilankibelo@gmail.com>
2023-07-12 10:27:07 +0200

    created form on register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,18 @@
+import { useNavigate } from "react-router-dom"
+import { Link } from "react-router-dom"
+
 const Register = () =>{
+    const navi = useNavigate();
+
+    const create =(()=>{
+        navi('/Home');
+    })
+
     return(
         <div className="bg-emerald-200 h-screen w-screen">
-            <section>
-                <h1 className="text-4xl">signup here</h1>
-                <div className="grid grid-cols-4">
+            <section className="h-full p-4 grid items-center">
+                <h1 className="text-9xl">signup here</h1>
+                <div className="grid grid-rows-4 h-4/5 pt-6">
                     <section>
                         <label>Fullname</label><br/>
                         <input type="text"/>
@@ -19,6 +28,13 @@ const Register = () =>{
                     <section>
                         <label>Confirm password</label><br/>
                         <input type="password"/>
+                    </section>
+                    <section>
+                        <button onClick={create}>signup</button>
+                        <div>
+                            <label>Already have an account?</label>
+                            <Link to={'/Login'}>Login</Link>
+                        </div>
                     </section>
                 </div>
             </section>
