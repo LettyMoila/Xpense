4699baf695077bfde7c493fe6c360a29e9f1def7
LettyMoila <moilankibelo@gmail.com>
2023-07-24 12:28:38 +0200

    adde data to add page

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,10 +1,55 @@
+import { useState } from "react"
 import Header from "../layouts/header"
+import data from "../data/data.json"
 
 const Add = ()=>{
+    const [expenses, setXpense] = useState(data);
+    const [item, setItem] = useState('');
+    const [cost, setCost] = useState('');
+    const [theDate, setDate] = useState('')
+
+    const save = (e) => {
+        e.preventDefault();
+
+        const newX = {
+            item: item,
+            theDate: theDate,
+            cost: cost
+        } 
+        setXpense([...expenses, newX])
+    }
+  
+
     return(
         
         <div className="h-screen w-screen bg-emerald-200">  
             <Header/>
+            <div className="flex gap-4 p-6 justify-center">
+                <section>
+                    <label>Item</label>
+                    <input type="text" onChange={(e) => setItem(e.target.value)}/>
+                </section>
+                <section>
+                    <label>Cost</label>
+                    <input type="" onChange={(e) => setCost(e.target.value)}/>
+                </section>
+                <section>
+                    <label>Date</label>
+                    <input type="date" onChange={(e) => setDate(e.target.value)}/>
+                </section>
+                <button onClick={save}>save</button>
+            </div>
+            <div className="flex gap-4 p-6 justify-center">
+                <ol className="grid gap-4">
+                    {expenses.map((saved) =>
+                        <li className="flex justify-between">
+                            {saved.item}
+                            {saved.cost}
+                            {saved.date}
+                        </li>
+                    )}
+                </ol>
+            </div>
         </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,11 @@ const Home = () =>{
     });
 
     const add=(()=>{
-        <input type="text"/>
+        navi('/Add')
+    });
+
+    const search=(()=>{
+        navi('/Search')
     });
 
     return(
@@ -18,7 +22,7 @@ const Home = () =>{
             <section className="flex gap-16 p-10 justify-center items-center h-4/5 ">
                 <div onClick={add} className="h-80 w-80 bg-emerald-100 rounded-md shadow-2xl">add</div>
                 <div onClick={status} className="h-80 w-80 bg-emerald-100 rounded-md shadow-2xl">view</div>
-                <div className="h-80 w-80 bg-emerald-100 rounded-md shadow-2xl">search</div>
+                <div onClick={search} className="h-80 w-80 bg-emerald-100 rounded-md shadow-2xl">search</div>
             </section>
             
         </div>
